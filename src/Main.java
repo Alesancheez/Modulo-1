d3909d142bb7902315367ee91a068437d919981e
Alesancheez <eliwsanz3@gmail.com>
2025-08-26 11:31:20 -0300

    actualizo clase4

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,6 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hola a todos");
-        }
+        System.out.println("Hola mundo");
     }
-
 }
---
+++ src/entity/Clase5.java
@@ -0,0 +1,38 @@
+package entity;
+
+public class Clase5 {
+
+    public static void main(String[] args) {
+     //1. Definir dos Integer y determinar cuál es mayor, cuál es menor o si son iguales.Imprimir el resultado obtenido.
+
+        Integer num1= 5;
+        Integer num2= 6;
+
+        int result = num1.compareTo(num2);
+        if (result == 0) System.out.println("Los numero son iguales");
+        else{ if (result>0)
+            System.out.println( num1 + "es mayor que " + num2);
+        else
+            System.out.println( num2 + "es mayor que " + num1);{
+
+
+
+
+     /*2.Crea una fecha de nacimiento y calcula la edad. Además imprimí por consola la edad, junto con la fecha formateada
+     de esta forma “YY MM DD”. */
+
+
+    //3. Convertir las variables Integer anteriores en String e imprimirlas por consola indicando su clase.
+
+
+    //4. Crea una función que reciba un nombre y retorne la inicial del mismo. Debe contar la cantidad de veces que se repite dicho carácter.
+
+
+    //5. Crear una función que reciba un int, lo convierta a String y compare con otro String en caso de ser iguales debe incluirse un mensaje.
+
+
+    //6. Crear una función que reciba un String y un número, y devuelva la letra que está en esa posición, junto con el largo total.
+
+
+    }
+}
