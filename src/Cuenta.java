2ec13f26cbe741e42eda64704bf1b4aa5c291b83
Alesancheez <eliwsanz3@gmail.com>
2025-09-23 11:55:25 -0300

    clase17

--- src/Cuenta.java
+++ src/Cuenta.java
@@ -1,25 +1,59 @@
+import java.util.ArrayList;
 public class Cuenta {
-
 
     private String  nombre;
     private Integer dni;
     private Double saldo;
     private Integer numeroDeCuenta;
-    private static final Double COMISION_FIJA = 0.05;
-    private static Double limiteExtraccion= 50.000;
+    private static final Double COMISION = 0.05;
+    private static Double limiteExtraccion= 50000.0;
 
-    public static void cuenta1(String nombre,Integer dni,Double saldo,Integer numeroDeCuenta){
+    public Cuenta (String nombre, Integer dni, Double saldo, Integer numeroDeCuenta){
 
+        this.numeroDeCuenta=numeroDeCuenta;
+        this.nombre=nombre;
+        this.dni=dni;
+        this.saldo=saldo;
     }
 
-    public double retirarMonto(Double num){
-        if (num >= saldo && num <= limiteExtraccion);
+    //CONSULTAR LIMITE EXTRACCION
+    public Double getLimiteExtraccion() {return  this.limiteExtraccion; }
+
+
+    //CONSULTAR NUMERO DE CUENTA
+    public Integer getNumeroDeCuenta(){
+        return this.numeroDeCuenta;
+    }
+
+    //CONSULTAR SALDO
+    public Double saldoActual(){
+        return this.saldo;
+    }
+
+    //RETIRAR SALDO
+    public String retirarSaldo (Double num){
+        if (num <= saldo && num<=limiteExtraccion){
             this.saldo = this.saldo - num;
             return "Retiro exitoso, tu saldo actual es de: " + saldo;
-        } else
-                return "No se pudo realizar el retiro, intente nuevamente con otro monto"
-    {
+        }else return "Fondos insuficientes.";
+    }
 
+    //TRANSFERENCIA
+    public String recibirSaldo ( Double monto ){
+        this.saldo = this.saldo + (monto-(monto*COMISION));
+        return "Saldo recibido: " + (monto-(monto*COMISION)) + " Saldo actual: " + this.saldo;
+    }
 
+    //TRANSFERIR
+    public String transferirSaldo (Double num, Integer numeroDeCuenta){
+        if (num <= saldo && num<=limiteExtraccion) {
+            this.saldo = this.saldo - num;
+            return "Transferencia exitosa, tu saldo actual es de: " + saldo;
+        }else return "Fondos insuficientes";
+    }
 
+    // CAMBIAR LIMITE EXTRACCION
+    public static void setLimiteExtraccion(Double nuevoLimite){
+        limiteExtraccion = nuevoLimite;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,42 @@
-import java.util.Scanner;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
 public class Main {
 
+    public static void main(String[] args) {
+        Cuenta cuenta1 = new Cuenta ("Pablo",45620074,8000.0,92333378);
+        Cuenta cuenta2 = new Cuenta("Bart",43629843,4000.0,92334566);
 
+        //VER LIMITE EXTRACCION
+        System.out.println(cuenta1.getLimiteExtraccion());
 
+        //CONSULTA NUMERO DE CUENTA
+        System.out.println(cuenta1.getNumeroDeCuenta());
 
+        //CONSULTA SALDO
+        System.out.println(cuenta1.saldoActual());
 
+        //RETIRAR SALDO
+        System.out.println(cuenta1.retirarSaldo(2000.0));
+
+        //RECIBIR TRANSFERENCIA
+        System.out.println(cuenta1.recibirSaldo(500.0));
+
+        //TRANSFERIR SALDO
+        System.out.println(cuenta1.transferirSaldo(9000.0,92334566));
+
+        //ARRAY - AGREGAR CUENTA
+        agregarCuenta(cuenta1);
+    }
+
+    //ARRAY
+    static ArrayList<Cuenta> cuentas= new ArrayList<>();
+
+    public static void agregarCuenta(Cuenta cuentas){
+        cuentas.add(Cuenta);
+        if(cuentas.size() == 5){
+            System.out.println("exedio limite perrmitido de cuentas creadas");
+        }else cuentas.add(Cuenta);
+
+    }
 
 }
 
